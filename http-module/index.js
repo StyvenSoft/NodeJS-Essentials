5262a393d8141d5cf7635ebaaf1bc9673baa26f4
Steveen <steveen.echeverri@gmail.com>
2020-03-10 22:56:20 -0500

    Example module http server

---
+++ http-module/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+// http.createServer([options][, requestListener])
+
+const handleServer = function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write('<h1>First test message!</h1>');
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function () {
+   console.log('Server on port 3000');
+});
